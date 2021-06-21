115ed80d2af9041f02751f93cb620a48ecd9d1f9
Alex Gaytsuk <gayrsuk@gmail.com>
2021-06-21 17:19:37 +0300

    function to aad custom recipe work

--- src/component/favorite-recipe/FavoriteRecipes.jsx
+++ src/component/favorite-recipe/FavoriteRecipes.jsx
@@ -4,14 +4,16 @@ import './favorite-recipe.scss';
 import RecipeCard from "../recipe-card";
 import AddCustomRecipe from "../add-custom-recipe";
 
-let randomArr = [];
-
 const FavoriteRecipes = () => {
     let storage = window.localStorage;
 
+    const [ text, setText ] = useState('');
+    const [ input, setInput ] = useState('');
     const [ popupFlag, setPopupFlag ] = useState(false);
-    const [ state, setState ] = useState([]);
 
+    const randomID = (min, max) => {
+        return  Math.round(min - 0.5 + Math.random() * (max - min + 1));
+    };
 
     const cardsRecipe = JSON.parse(storage.meal)
         .map(meal => {
@@ -32,15 +34,26 @@ const FavoriteRecipes = () => {
             )
         });
 
-    const saveRecipeToLocalStorage = (event) => {
+    const saveRecipeToLocalStorage = event => {
         event.preventDefault();
-        randomArr.push(state);
-        let data = JSON.stringify(randomArr);
+        let obj = {
+            idMeal: randomID(1, 9999),
+            strMeal: input,
+            strInstructions: text,
+            strMealThumb: 'https://lh3.googleusercontent.com/proxy/bScjZ5fwTM1-6tBBxJWNi7QSK5acwpfcM7i4kClRD8vJIEHMXZZzFzYO_HMCCYEbJLXUraHE1pY_PdQsZymdwBKx'
+        };
+        let tempArr = [...JSON.parse(storage.meal), obj];
+        let data = JSON.stringify(tempArr);
         storage.setItem("meal", data);
         setPopupFlag(false);
+    };
 
-        console.log('storage save: ', JSON.parse(storage.getItem('meal')));
-        console.log('randomArr save: ', randomArr);
+    const handleInput = event => {
+        setInput(event.target.value);
+    };
+
+    const handleTextArea = event => {
+        setText(event.target.value);
     };
 
     return (
@@ -60,6 +73,7 @@ const FavoriteRecipes = () => {
                             type="text"
                             placeholder='Dish title'
                             className='custom-dish_input'
+                            onChange={handleInput}
                         />
                         <textarea
                             className='custom-dish_text-field'
@@ -67,6 +81,7 @@ const FavoriteRecipes = () => {
                             cols="30"
                             rows="10"
                             placeholder='Dish description...'
+                            onChange={handleTextArea}
                         >
 
                         </textarea>
