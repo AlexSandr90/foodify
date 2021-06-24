71c0149dc6292d3c204de8f49464ecbbdcd5c95f
Alexandr Gaytsuk <gayrsuk@gmail.com>
2021-06-24 11:38:41 +0300

    refactored

--- src/component/app/App.jsx
+++ src/component/app/App.jsx
@@ -17,14 +17,12 @@ import {
 const App = () => {
 
     const renderCondition = () => {
-        if ( (isEmpty('meal') && JSON.parse( storage.getItem('meal') ).length !== 0) === true) {
+        if ( isEmpty('meal') && JSON.parse( storage.getItem('meal') ).length !== 0) {
             return <FavoriteRecipes/>
         } else {
             return <Redirect to='/'/>
         }
     };
-
-    console.log(isEmpty() && JSON.parse(storage.getItem('meal')).length !== 0);
 
     return (
         <HashRouter basename={process.env.PUBLIC_URL}>
